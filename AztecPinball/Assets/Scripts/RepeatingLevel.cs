55b15da0c03ce4dc31892e84e22f7542d20f4f4a
Laharnar <marko.laharnar@gmail.com>
2018-12-01 21:05:55 +0100

    trap generator

--- AztecPinball/Assets/Scripts/RepeatingLevel.cs
+++ AztecPinball/Assets/Scripts/RepeatingLevel.cs
@@ -17,61 +17,74 @@ public class RepeatingLevel : MonoBehaviour {
 
     // Use this for initialization
     void Start () {
-        Spawn(startPoint.transform.position);
+        numOfSpawnPointsOnStairs = startPoint.childCount;
+        for (int i = 0; i < 10; i++) {
+            Spawn(startPoint.transform.position);
+            startPoint.transform.position += (refPoint2.position - refPoint1.position);
+            PickTraps(spawnedStairs[i]);
+        }
+
+
     }
 
     public void Spawn(Vector2 pos) {
         Vector3 start = pos;
-        for (int i = 0; i < 10; i++) {
-            Transform t = Instantiate(stairPref);
-            t.position = start;
-            start += (refPoint2.position - refPoint1.position);
 
-            startPoint.transform.position = t.position;
-            spawnedStairs.Add(t);
-        }
-    }
-    public void SpawnObstacle(Transform stairs) {
+        // spawn one stair
+        Transform stair = Instantiate(stairPref);
+        stair.position = start;
+        start += (refPoint2.position - refPoint1.position);
 
-        // check if pos is already taken
-        Vector3 pos = stairs.GetChild(Random.Range(0, numOfSpawnPointsOnStairs)).position;
-        positions.Add(pos);
-        for (int i = 0; i < positions.Count; i++) {
-            if (positions[i] == pos) {
-                return;
+        startPoint.transform.position = stair.position;
+        spawnedStairs.Add(stair);
+
+        // spawn obstacles for stairs
+        // all obstacles as children, disabled.
+        for (int i = 0; i < obstaclePrefs.Length; i++) {
+            for (int k = 0; k < numOfSpawnPointsOnStairs; k++) {
+                // in scene, stairs have n child empty game objects for references where traps can spawn.
+                Transform obstacle = Instantiate(obstaclePrefs[i],
+                    stair.GetChild(k).position, new Quaternion());
+                obstacle.gameObject.SetActive(false);
+                obstacle.gameObject.transform.parent = stair.GetChild(k);
             }
         }
+        
+    }
 
-        // in scene, stairs have 2 child empty game objects for references where stuff can spawn.
-        Instantiate(obstaclePrefs[Random.Range(0, obstaclePrefs.Length)],
-            pos, new Quaternion());
+    public void PickTraps(Transform stair) {
+        int n = numOfSpawnPointsOnStairs; // reduce to make stairs emptier
+        float trapAppearChance = 0.5f;
 
-        // some terrible code.
-        if (positions.Count > 100) {
-            for (int i = 0; i < 10; i++) {
-                positions.RemoveAt(0);
+        for (int i = 0; i < numOfSpawnPointsOnStairs; i++) {
+            Transform t = stair.GetChild(i);
+            // disable all traps, then activate N, trap or nothing.
+            for (int k = 0; k < t.childCount; k++) {
+                t.GetChild(k).gameObject.SetActive(false);
+            }
+            if (Random.Range(0f, 1f) >= trapAppearChance) {
+                t.GetChild(Random.Range(0, t.childCount)).gameObject.SetActive(true);
             }
         }
     }
+    
     // Update is called once per frame
     void Update () {
         // TODO: camera should follow falling body, not move in dir.
         Camera.main.transform.Translate(
             (refPoint2.position - refPoint1.position).normalized * speed * Time.deltaTime, Space.World);
 
-        for (int i = 0; i < spawnedStairs.Count; i++) {
-            if (spawnedStairs[i].position.y > Camera.main.transform.position.y -20) {
-                for (int j = 0; j < numOfSpawnPointsOnStairs; j++) {
-                    SpawnObstacle(spawnedStairs[i].transform);
-                }
-            }
-        }
+        // move stairs at the top to the bottom.
         for (int i = 0; i < numOfSpawnPointsOnStairs; i++) {
             if (spawnedStairs[i].position.y > Camera.main.transform.position.y+10) {
                 spawnedStairs[i].position = spawnedStairs[spawnedStairs.Count - 1].position + (refPoint2.position - refPoint1.position);
                 spawnedStairs.Add(spawnedStairs[i]);
                 spawnedStairs.RemoveAt(i);
 
+                // Update traps.
+                for (int k = 0; k < spawnedStairs.Count; k++) {
+                    PickTraps(spawnedStairs[spawnedStairs.Count-1]);
+                }
             }
         }
 	}
