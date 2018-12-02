408da26864bc3fedcb535f5b7a5fd59661b90fa7
grudwa <andraz.gruden@gmail.com>
2018-12-02 08:57:12 +0100

    fixed ui

--- AztecPinball/Assets/Scripts/UIManager.cs
+++ AztecPinball/Assets/Scripts/UIManager.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
@@ -8,10 +6,24 @@ public class UIManager : MonoBehaviour {
 
     //Main camera must have ListenerMainCamera attached to check if it has to turn off sound
 
+    public Button soundBtn;
+    public GameObject tutorialCanvas;
+    bool toggleActivator;
+
 
     private void Start()
     {
-        //if listener is 1 this means that sound is on...
+        bool toggleActivator = false;
+        //if listener is 1 this means that sound is on...otherwise the sound is off
+        if (PlayerPrefs.GetInt("listener") == 1)
+        {
+            soundBtn.image.color = Color.white;
+        }
+        else if (PlayerPrefs.GetInt("listener") == 0)
+        {
+            soundBtn.image.color = Color.red;
+        }
+
     }
 
     public void SceneLoader(string SceneToLoad)
@@ -35,13 +47,26 @@ public class UIManager : MonoBehaviour {
         if(PlayerPrefs.GetInt("listener") == 1)
         {
             PlayerPrefs.SetInt("listener", 0);
+            soundBtn.image.color = Color.red;
+
+
             Debug.Log(PlayerPrefs.GetInt("listener"));
         }
         else
         {
             PlayerPrefs.SetInt("listener", 1);
+            soundBtn.image.color = Color.white;
+
+
             Debug.Log(PlayerPrefs.GetInt("listener"));
         }
        
     }
+
+    public void TutorialEnabler()
+    {
+        toggleActivator = !toggleActivator;
+        tutorialCanvas.SetActive(toggleActivator);
+    }
+
 }
