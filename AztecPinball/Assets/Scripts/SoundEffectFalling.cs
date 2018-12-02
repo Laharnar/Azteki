09857b9bc98669ec68628eda487628a3746fba40
grudwa <andraz.gruden@gmail.com>
2018-12-02 18:08:14 +0100

    fix sounda array

--- AztecPinball/Assets/Scripts/SoundEffectFalling.cs
+++ AztecPinball/Assets/Scripts/SoundEffectFalling.cs
@@ -19,7 +19,7 @@ public class SoundEffectFalling : MonoBehaviour {
         if(collision.gameObject.tag == "obstacle")
         {
             //here he will scream
-            audio.clip = screamClips[Random.Range(0, fallClips.Length)];
+            audio.clip = screamClips[Random.Range(0, fallClips.Length-1)];
                 audio.Play();
            
         }
@@ -27,7 +27,7 @@ public class SoundEffectFalling : MonoBehaviour {
         {
             if (audio.isPlaying == false)
             {
-                audio.clip = fallClips[Random.Range(0, fallClips.Length)];
+                audio.clip = fallClips[Random.Range(0, fallClips.Length-1)];
                 audio.Play();
             }
         }
